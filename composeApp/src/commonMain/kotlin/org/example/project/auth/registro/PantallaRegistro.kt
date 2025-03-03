0ca0051255eb7e844f1895d7d34d09328cbe4f20
SISA0729 <ismael.sisa@educa.madrid.org>
2025-03-03 19:42:40 +0100

    :)

--- composeApp/src/commonMain/kotlin/org/example/project/auth/registro/PantallaRegistro.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/auth/registro/PantallaRegistro.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -17,8 +21,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import org.example.project.common.componentes.CtextField
 import org.example.project.estilos.BotonHeight
@@ -44,14 +55,25 @@ fun PantallaRegistro(
                 }
             )
             .padding(
-                start = 16.dp,
-                end = 16.dp,
-                top = 16.dp,
+                start = 18.dp,
+                end = 18.dp,
+                top = 100.dp,
                 bottom = 16.dp
             ),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
+        Text(
+            text = "Crear cuenta",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF1E88E5),
+            style = MaterialTheme.typography.h4,
+            textAlign = TextAlign.Left,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+
+
         CtextField(
             value = uiState.username,
             onValueChange = onUsernameChange,
@@ -59,12 +81,14 @@ fun PantallaRegistro(
             placeholderText = "Usuario"
 
         )
+
         CtextField(
             value = uiState.correo,
             onValueChange = onCorreoChange,
             keyboardType = KeyboardType.Email,
             placeholderText = "Correo"
         )
+
         CtextField(
             value = uiState.password,
             onValueChange = onPasswordChange,
@@ -73,10 +97,9 @@ fun PantallaRegistro(
             placeholderText = "Contraseña"
         )
 
+
         Button(
             onClick = {
-
-                navController.navigate("inicio")
 
             },
             modifier = modifier
@@ -88,6 +111,36 @@ fun PantallaRegistro(
             shape = MaterialTheme.shapes.medium
         ){
             Text(text = "Registrarse")
+        }
+
+        Column(
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "¿Ya tienes una cuenta?",
+                    color = if (isSystemInDarkTheme()) Color.White else MaterialTheme.colors.onSurface,
+                    fontSize = 16.sp
+                )
+
+                Spacer(modifier = Modifier.width(5.dp))
+
+                ClickableText(
+                    text = AnnotatedString(
+                        "Inciar Sesión",
+                        spanStyle = SpanStyle(
+                            color = Color.Blue,
+                            textDecoration = TextDecoration.None
+                        )
+                    ),
+                    onClick = {
+                        navController.navigate("inicio")
+                    }
+                )
+            }
         }
     }
 }
